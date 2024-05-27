a2b4fc5abb6efdb68a951e16fa38b7e6d0d803b3
LAPTOP-5KG484I3\santi <david.castiblanco@softgic.co>
2024-05-27 06:21:39 -0500

    SpaceShip Postgres
    Metodos completos con Testunitarios

--- src/main/java/com/example/spaceship/infrastructure/repository/SpaceShipRepositoryPostgres.java
+++ src/main/java/com/example/spaceship/infrastructure/repository/SpaceShipRepositoryPostgres.java
@@ -21,57 +21,69 @@ public class SpaceShipRepositoryPostgres implements SpaceShipRepository {
 
     @Autowired
     JdbcTemplate template;
-    
+
     @Autowired
     ProductionRepositoryPostgres productionRepository;
 
     static final String SQL_FIND_ALL_SPACESHIPS = "SELECT id, name, type, last_captain, faction FROM spaceship LIMIT ? OFFSET ?";
-    
-    static final String SQL_FIND_SPACESHIP_BY_ID = "SELECT id, name, type, last_captain, faction FROM spaceship WHERE id = ?";
-    
-     static final String SQL_FIND_SPACESHIP_BY_NAME_CONTAINING = "SELECT id, name, type, last_captain, faction FROM spaceship WHERE name LIKE ?";
 
-     
+    static final String SQL_FIND_SPACESHIP_BY_ID = "SELECT id, name, type, last_captain, faction FROM spaceship WHERE id = ?";
+
+    static final String SQL_FIND_SPACESHIP_BY_NAME_CONTAINING = "SELECT id, name, type, last_captain, faction FROM spaceship WHERE name LIKE ?";
+
+    private static final String SQL_INSERT_SPACESHIP = "INSERT INTO spaceship (id, name, type, last_captain, faction) VALUES (?, ?, ?, ?, ?)";
+
+    private static final String SQL_UPDATE_SPACESHIP = "UPDATE spaceship SET name = ?, type = ?, last_captain = ?, faction = ? WHERE id = ?";
+
+    private static final String SQL_DELETE_SPACESHIP = "DELETE FROM spaceship WHERE id = ?";
+    
+    private static final String SQL_DELETE_SPACESHIP_PRODUCTION = "DELETE FROM spaceship_production WHERE spaceship_id = ?";
+    
     @Override
     public List<SpaceShip> findAll(int offset, int limit) {
-        return template.query(SQL_FIND_ALL_SPACESHIPS, new SpaceShipRowMapper(productionRepository),limit,offset);
+        return template.query(SQL_FIND_ALL_SPACESHIPS, new SpaceShipRowMapper(productionRepository), limit, offset);
     }
 
     @Override
     public Optional<SpaceShip> findById(String id) {
-         List<SpaceShip> spaceships = template.query(SQL_FIND_SPACESHIP_BY_ID, new SpaceShipRowMapper(productionRepository), id);
+        List<SpaceShip> spaceships = template.query(SQL_FIND_SPACESHIP_BY_ID, new SpaceShipRowMapper(productionRepository), id);
         return spaceships.stream().findFirst();
     }
 
     @Override
     public List<SpaceShip> findByNameContaining(String name) {
         String searchPattern = "%" + name + "%";
-        return template.query(SQL_FIND_SPACESHIP_BY_NAME_CONTAINING, new SpaceShipRowMapper(productionRepository),searchPattern);
+        return template.query(SQL_FIND_SPACESHIP_BY_NAME_CONTAINING, new SpaceShipRowMapper(productionRepository), searchPattern);
     }
 
     @Override
     public void create(SpaceShip ship) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        template.update(SQL_INSERT_SPACESHIP, ship.id(), ship.name(), ship.type(), ship.lastCaptain(), ship.faction());
+        productionRepository.saveProductions(ship.appearances(), ship.id());
     }
 
     @Override
     public void update(SpaceShip ship) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        template.update(SQL_UPDATE_SPACESHIP, ship.name(), ship.type(), ship.lastCaptain(), ship.faction(), ship.id());
+        template.update("DELETE FROM spaceship_production WHERE spaceship_id = ?", ship.id());
+        productionRepository.saveProductions(ship.appearances(),ship.id());
     }
 
     @Override
     public void delete(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        
+        template.update(SQL_DELETE_SPACESHIP_PRODUCTION, id);
     
-  private static class SpaceShipRowMapper implements RowMapper<SpaceShip> {
+        template.update(SQL_DELETE_SPACESHIP, id);
+    }
 
-         private final ProductionRepositoryPostgres productionRepositoryPostgres;
+    private static class SpaceShipRowMapper implements RowMapper<SpaceShip> {
+
+        private final ProductionRepositoryPostgres productionRepositoryPostgres;
 
         public SpaceShipRowMapper(ProductionRepositoryPostgres productionRepositoryPostgres) {
             this.productionRepositoryPostgres = productionRepositoryPostgres;
         }
-
 
         @Override
         public SpaceShip mapRow(ResultSet rs, int rowNum) throws SQLException {
--- src/test/java/com/example/spaceship/infrastructure/repository/SpaceShipRepositoryPostgresTest.java
+++ src/test/java/com/example/spaceship/infrastructure/repository/SpaceShipRepositoryPostgresTest.java
@@ -4,6 +4,7 @@
  */
 package com.example.spaceship.infrastructure.repository;
 
+import com.example.spaceship.domain.model.Production;
 import com.example.spaceship.domain.model.SpaceShip;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -40,10 +43,12 @@ public class SpaceShipRepositoryPostgresTest {
         postgreSQLContainer.start();
     }
 
-
     @Autowired
     SpaceShipRepositoryPostgres repository;
 
+     @SpyBean
+     JdbcTemplate jdbcTemplate;
+     
     @Test
     public void testFindById() {
         var actual = repository.findById("1");
@@ -54,16 +59,16 @@ public class SpaceShipRepositoryPostgresTest {
         assertEquals("Federation", actual.get().faction());
     }
 
-   @Test
+    @Test
     public void testFindByNameContaining() {
         var actual = repository.findByNameContaining("USS");
         assertEquals(2, actual.size());
         assertEquals("Starship", actual.get(0).type());
         assertEquals("Explorer", actual.get(1).type());
     }
-    
+
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         var actual = repository.findAll(2, 3);
         assertEquals(3, actual.size());
         assertEquals("Defiant", actual.get(0).name());
@@ -71,5 +76,46 @@ public class SpaceShipRepositoryPostgresTest {
         assertEquals("Prometheus", actual.get(2).name());
     }
 
+    @Test
+    public void testCreateSpaceShip() {
+        SpaceShip ship = new SpaceShip("8", "VoLVO", List.of(
+                new Production.Movie("Star Trek: The Motion Picture", (short) 1979),
+                new Production.Serie("Star Trek: Voyager", (short) 7),
+                new Production.Serie("Star Trek: The Next Generation", (short) 7)
+        ), "Starship", "Kathryn Janeway", "Federation");
+
+        repository.create(ship);
+
+        Optional<SpaceShip> savedShip = repository.findById("8");
+        assertTrue(savedShip.isPresent());
+        assertEquals("VoLVO", savedShip.get().name());
+        assertEquals(3, savedShip.get().appearances().size());
+    }
+
+    @Test
+    public void testUpdateSpaceShip() {
+        SpaceShip ship = new SpaceShip("1", "USS Voyager Updated", List.of(
+                new Production.Movie("Star Trek: The Motion Picture", (short) 1979),
+                new Production.Serie("Star Trek: Voyager", (short) 7)
+        ), "Starship", "Kathryn Janeway", "Federation");
+
+        repository.update(ship);
+
+        Optional<SpaceShip> updatedShip = repository.findById("1");
+        assertTrue(updatedShip.isPresent());
+        assertEquals("USS Voyager Updated", updatedShip.get().name());
+        assertEquals(2, updatedShip.get().appearances().size());
+    }
+
+    @Test
+    public void testDeleteSpaceShip() {
+
+        repository.delete("2");
+
+        Optional<SpaceShip> deletedShip = repository.findById("2");
+        assertFalse(deletedShip.isPresent());
+        List<Integer> productionIds = jdbcTemplate.queryForList("SELECT production_id FROM spaceship_production WHERE spaceship_id = ?", Integer.class, "2");
+        assertTrue(productionIds.isEmpty());
+    }
 
 }
